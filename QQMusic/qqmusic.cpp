0cf6336034cb7ca4d6f2bb4c2d80a7f719742edc
huang-qiruiqq <3208137151@qq.com>
2026-03-01 19:55:07 +0800

    给窗口设置阴影效果

--- QQMusic/qqmusic.cpp
+++ QQMusic/qqmusic.cpp
@@ -1,6 +1,8 @@
 #include "qqmusic.h"
 #include "ui_qqmusic.h"
+#include <QPushButton>
 #include <QMouseEvent>
+#include <QGraphicsDropShadowEffect>
 
 QQMusic::QQMusic(QWidget *parent)
     : QWidget(parent)
@@ -19,6 +21,16 @@ QQMusic::~QQMusic()
 void QQMusic::initUI()
 {
     this->setWindowFlag(Qt::FramelessWindowHint);
+
+    // 实现窗口阴影效果之前需要先设置窗口背景透明
+    setAttribute(Qt::WA_TranslucentBackground);
+
+    // 实现窗口阴影效果
+    QGraphicsDropShadowEffect* shadowEffect = new QGraphicsDropShadowEffect(this);
+    shadowEffect->setOffset(0,0);
+    shadowEffect->setColor("#000000"); // 黑色
+    shadowEffect->setBlurRadius(20);
+    this->setGraphicsEffect(shadowEffect);
 }
 
 
