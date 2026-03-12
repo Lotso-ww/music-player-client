fd6d2f2fc29fb37a065540d525fc24e9cc9f03a5
huang-qiruiqq <3208137151@qq.com>
2026-03-12 11:09:40 +0800

    向上向下翻页功能实现 && 处理图片索引功能

--- QQMusic/recbox.cpp
+++ QQMusic/recbox.cpp
@@ -46,10 +46,26 @@ void RecBox::initRecBoxUi(QJsonArray data, int row)
 
 void RecBox::createRecItem()
 {
+    // 溢出掉之前的旧元素
+    QList<RecBoxItem*> recUpList = ui->recListUp->findChildren<RecBoxItem*>();
+    for(auto& e : recUpList)
+    {
+        ui->recListUpLayout->removeWidget(e);
+        delete e;
+    }
+
+    QList<RecBoxItem*> recDownList = ui->recListDown->findChildren<RecBoxItem*>();
+    for(auto& e : recDownList)
+    {
+        ui->recListDownLayout->removeWidget(e);
+        delete e;
+    }
+
     // 创建RecBoxItem对象，往RecBox里面添加
     // 今日为你推荐 row = 1, col = 4;
     // 音乐补给站: row = 2, col = 8;
-    for(int i = 0; i < col; i++)
+    int index = 0; // 方便分今日推荐和音乐补给站的item数量
+    for(int i = currentIndex * col; i < col + currentIndex * col; i++)
     {
         RecBoxItem* item = new RecBoxItem();
 
@@ -64,7 +80,7 @@ void RecBox::createRecItem()
         // 如果是recMusicBox：row为1，只能执⾏else，所有4个RecBoxItem都添加到ui->recListUpHLayout中
         // 如果是supplyMuscBox：row为2，col为8，col/2结果为4，i为0 1 2 3时，元素添加到ui->recListDownHLayout中
         // i为4 5 6 7时，元素添加到ui->recListUpHLayout中
-        if(2 == row && i >= col / 2)
+        if(2 == row && index >= col / 2)
         {
             ui->recListDownLayout->addWidget(item);
         }
@@ -72,16 +88,30 @@ void RecBox::createRecItem()
         {
             ui->recListUpLayout->addWidget(item);
         }
-    }
-}
 
-void RecBox::on_btUp_clicked()
-{
-    // 点击btUp按钮，显⽰前4张图⽚，如果已经是第⼀张图⽚，循环从后往前显⽰
+        index++;
+    }
 }
 
 void RecBox::on_btDown_clicked()
 {
-    // 点击btUp按钮，显⽰前8张图⽚，如果已经是第⼀张图⽚，循环从后往前显⽰
+    // 点击按钮，需要显示下一组图片，如果已经是最后一组图⽚，显示第0组图片
+    currentIndex++;
+    if(currentIndex >= count)
+        currentIndex = 0;
+
+    createRecItem();
 }
+
+void RecBox::on_btUp_clicked()
+{
+    // 点击按钮，需要显示上一组图片，如果已经是第0组图片，显示最后一组图片
+    currentIndex--;
+    if(currentIndex < 0)
+        currentIndex = count - 1;
+
+    createRecItem();
+}
+
+
 
