f0a685b1af02ec58c5d03d75a72ca04562fa7f2c
huang-qiruiqq <3208137151@qq.com>
2026-03-17 14:02:49 +0800

    窗口透明 + 阴影

--- QQMusic/volumetool.cpp
+++ QQMusic/volumetool.cpp
@@ -1,5 +1,6 @@
 #include "volumetool.h"
 #include "ui_volumetool.h"
+#include <QGraphicsDropShadowEffect>
 
 VolumeTool::VolumeTool(QWidget *parent) :
     QWidget(parent),
@@ -7,7 +8,19 @@ VolumeTool::VolumeTool(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    setWindowFlag(Qt::Popup);
+    setWindowFlags(Qt::Popup | Qt::FramelessWindowHint | Qt::NoDropShadowWindowHint);
+
+    // 在windows上，设置透明效果后，窗⼝需要加上Qt::FramelessWindowHint格式
+    // 否则没有控件位置的背景是⿊⾊的
+    // 由于默认窗⼝有阴影，因此还需要将窗⼝的原有的阴影去掉，窗⼝需要加上 Qt::NoDropShadowWindowHint
+    setAttribute(Qt::WA_TranslucentBackground);
+
+    // ⾃定义阴影效果
+    QGraphicsDropShadowEffect* shadowEffect = new QGraphicsDropShadowEffect(this);
+    shadowEffect->setColor("#646464");
+    shadowEffect->setOffset(0, 0);
+    shadowEffect->setBlurRadius(10);
+    this->setGraphicsEffect(shadowEffect);
 }
 
 VolumeTool::~VolumeTool()
