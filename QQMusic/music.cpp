8198a3b408462d708c892e606eb229efc04fcc1c
huang-qiruiqq <3208137151@qq.com>
2026-03-19 17:22:34 +0800

    Music类添加成员变量 && set和get系列方法 && 设置musicId

--- QQMusic/music.cpp
+++ QQMusic/music.cpp
@@ -1,14 +1,83 @@
 #include "music.h"
-
+#include <QUuid>
 
 Music::Music()
+     : isLike(false)
+     , isHistory(false)
 {
 
 }
 
-Music::Music(QUrl url)
+Music::Music(const QUrl& url)
+     : isLike(false)
+     , isHistory(false)
+     , musicUrl(url)
 {
     // 读取url对应的歌曲文件信息，解析出元数据
     // 歌曲名称，歌曲作者，歌曲专辑，歌曲持续时长
+    musicId = QUuid::createUuid().toString();
+}
 
+// set系列
+void Music::setMusicName(const QString& musicName)
+{
+    this->musicName = musicName;
+}
+void Music::setMusicSinger(const QString& musicSinger)
+{
+    this->musicSinger = musicSinger;
+}
+void Music::setMusicAlbumn(const QString& musicAlbumn)
+{
+    this->musicAlbumn = musicAlbumn;
+}
+void Music::setMusicDuration(const qint64 musicDuration)
+{
+    this->musicDuration = musicDuration;
+}
+void Music::setIsLike(bool isLike)
+{
+    this->isLike = isLike;
+}
+void Music::setIsHistory(bool isHistory)
+{
+    this->isHistory = isHistory;
+}
+void Music::setMusicUrl(const QUrl& musicUrl)
+{
+    this->musicUrl = musicUrl;
+}
+
+// get系列
+QString Music::getMusicId()const
+{
+    return musicId;
+}
+QString Music::getMusicName()const
+{
+    return musicName;
+}
+QString Music::getMusicSinger()const
+{
+    return musicSinger;
+}
+QString Music::getMusicAlbumn()const
+{
+    return musicAlbumn;
+}
+qint64 Music::getMusicDuration()const
+{
+    return musicDuration;
+}
+bool Music::getIsLike()const
+{
+    return isLike;
+}
+bool Music::getIsHistory()const
+{
+    return isHistory;
+}
+QUrl Music::getMusicUrl()const
+{
+    return musicUrl;
 }
--- QQMusic/music.h
+++ QQMusic/music.h
@@ -7,7 +7,44 @@ class Music
 {
 public:
     Music();
-    Music(QUrl url);
+    Music(const QUrl& url);
+
+public:
+    // set系列
+    void setMusicName(const QString& musicName);
+    void setMusicSinger(const QString& musicSinger);
+    void setMusicAlbumn(const QString& musicAlbumn);
+    void setMusicDuration(const qint64 musicDuration);
+    void setIsLike(bool isLike);
+    void setIsHistory(bool isHistory);
+    void setMusicUrl(const QUrl& musicUrl);
+
+    // get系列
+    QString getMusicId()const;
+    QString getMusicName()const;
+    QString getMusicSinger()const;
+    QString getMusicAlbumn()const;
+    qint64 getMusicDuration()const;
+    bool getIsLike()const;
+    bool getIsHistory()const;
+    QUrl getMusicUrl()const;
+
+private:
+    // 为了保证歌曲的唯一性，需要给音乐添加id
+    // 磁盘上的歌曲⽂件经常删除或者修改位置，导致播放时找不到⽂件，或者重复添加
+    // 此处⽤musicId来维护播放列表中⾳乐的唯⼀性
+    QString musicId;
+
+    // 基本信息: 歌曲名称 歌曲作者 歌曲专辑 歌曲持续时长
+    QString musicName;
+    QString musicSinger;
+    QString musicAlbumn;
+    qint64 musicDuration; // qint64 -- long long
+
+    // 其它: 歌曲是否喜欢 歌曲是否为历史播放 歌曲Url
+    bool isLike;
+    bool isHistory;
+    QUrl musicUrl;
 };
 
 #endif // MUSIC_H
