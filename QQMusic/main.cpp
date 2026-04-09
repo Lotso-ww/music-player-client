ece41e03bb8b35b486dac91744cc90830bc7f353
huang-qiruiqq <3208137151@qq.com>
2026-04-09 17:27:25 +0800

    只允许运行一个实例的实现

--- QQMusic/main.cpp
+++ QQMusic/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     // 当程序第一次运行时,由于共享内存还没创建出来,所以一定会失败
     // 当程序第二次运行时,由于共享内存已经被前一次运行的时候申请好,所以第二次一定会成功
-    // 由于只能运行一定实例,所以我们直接让第二个实例直接退出即可
+    // 由于只能运行一定实例,所以我们直接让第二个实例直接退出即可.
     if(shareMemory.attach())
     {
         QMessageBox::information(nullptr, "QQMusic提示", "QQMusic已经在运行");
